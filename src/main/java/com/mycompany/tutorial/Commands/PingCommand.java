0333e5ad30f04ef2b01bed6830c0bfc0b63f2799
Mahanvali <Mahanvali@users.noreply.github.com>
2024-09-30 18:52:05 +0330

    EPISODE 11

--- src/main/java/com/mycompany/tutorial/Commands/PingCommand.java
+++ src/main/java/com/mycompany/tutorial/Commands/PingCommand.java
@@ -1,6 +1,9 @@
 package com.mycompany.tutorial.Commands;
 
 
+import java.awt.Color;
+
+import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
@@ -8,10 +11,16 @@ public class PingCommand extends ListenerAdapter {
     @Override
     public void onSlashCommandInteraction(SlashCommandInteractionEvent event){
         //  Ping slash command
+        Color embedColor = new Color(44, 45, 48);
         if(event.getName().equals("ping")){ 
             //  Add subcomamnd
             if(event.getSubcommandName().equals("add")){
-                event.reply("Add initiated").queue();
+               EmbedBuilder embedBuilder = new EmbedBuilder();
+               embedBuilder.setTitle("Add initiated");
+               embedBuilder.setDescription("This is a description");
+               embedBuilder.setFooter("This is a simple JDA Bot", event.getJDA().getSelfUser().getDefaultAvatarUrl());
+               embedBuilder.setColor(embedColor);
+               event.replyEmbeds(embedBuilder.build()).queue();
             }
             // Remove subcommand
             if(event.getSubcommandName().equals("remove")){
